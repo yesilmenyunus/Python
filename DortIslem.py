ff7eacb5dff23da800135f6a246de69bf3b8e98b
Yunus YEŞİLMEN <ynsyslmn@hotmail.com>
2023-08-12 13:44:03 +0300

    Dört İşlem

---
+++ DortIslem.py
@@ -0,0 +1,8 @@
+# Girilen değeri sayı tipine dönüştürüp değişkenlere atıyoruz
+sayı1 = float(input("İlk sayıyı giriniz: "))
+sayı2 = float(input("İkinci sayıyı giriniz: "))
+
+print("{} ve {} sayılarının toplamı: {}".format(sayı1,sayı2,sayı1+sayı2))
+print("{} ve {} sayılarının farkı: {}".format(sayı1,sayı2,sayı1-sayı2))
+print("{} ve {} sayılarının çarpımı: {}".format(sayı1,sayı2,sayı1*sayı2))
+print("{} sayısını {} sayısına böldüğümüzde sonuç: {}".format(sayı1,sayı2,sayı1/sayı2))
